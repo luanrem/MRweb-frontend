3ad60fda0c81c19c3d8e3d325de19acf7e110726
Luan Roberto Estrada Martins <luanrem@gmail.com>
2020-07-25 10:58:32 -0300

    Config default routes and history lib

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Route from './Route';
+
+import Main from '../pages/Main';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import System from '../pages/System';
 
 export default function routes() {
   return (
     <Switch>
-
+      <Route path="/" exact component={Main}/>
+      <Route path="/login" component={SignIn}/>
+      <Route path="/register" component={SignUp}/>
+      <Route path="/system" component={System}/>
     </Switch>
   );
 }
