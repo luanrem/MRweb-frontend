ec02e2a0c8869e9b2c9f9d67d59a5debe367c2f7
Luan Roberto Estrada Martins <luanrem@gmail.com>
2020-07-25 11:04:23 -0300

    Reactotron config and bug solve

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Route from './Route';
 
 import Main from '../pages/Main';
+import System from '../pages/System';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import System from '../pages/System';
 
 export default function routes() {
   return (
     <Switch>
       <Route path="/" exact component={Main}/>
+
       <Route path="/login" component={SignIn}/>
       <Route path="/register" component={SignUp}/>
       <Route path="/system" component={System}/>
