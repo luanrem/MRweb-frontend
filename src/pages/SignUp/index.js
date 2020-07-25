986826a52a717dcc9a29c2f97b54568149a64878
Luan Roberto Estrada Martins <luanrem@gmail.com>
2020-07-25 11:21:56 -0300

    Private Routes configured

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function SignUp() {
+  return (
+    <div>SignUp</div>
+  );
+}
+
+export default SignUp;
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
 
 // import { Container } from './styles';
 
 export default function RouteWrapper({
-  component: Component,
+  // These are the properties that Route are passing with Route in ./index.js
+  component: Component, // The component we call Component with uper case C
+  isPrivate = false, // This property say what component neet to be loged-in
   ...rest
 }) {
+  const signed = false;
+
+  // If the user is not signed and the route is private, redirect to main page
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />
+  }
+
+  // If the user is signed and the route is not private, redirect to system route
+  if (signed && !isPrivate) {
+    return <Redirect to="/system" />
+  }
+
+  // If the user is signed and the route is private, than return:
   return (
     <Route
       {...rest}
+      component={Component}
     />
   );
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+  .isRequired,
+}
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
 }
